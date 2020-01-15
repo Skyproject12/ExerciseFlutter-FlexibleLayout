a5d5c20b3de22d0c3e34128a7971cde4fb3743ca
Kadek Sumendra <sumendrakadek108@gmail.com>
2020-01-15 10:56:13 +0700

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Flex Layout"),
+        ),
+        body: Column(
+          children: <Widget>[
+            Flexible(
+              flex: 1,
+              child: Row(
+                children: <Widget>[
+                  Flexible(
+                      flex: 1,
+                      child: Container(
+                        margin: EdgeInsets.all(5),
+                        color: Colors.red,
+                      )),
+                  Flexible(
+                      flex: 1,
+                      child: Container(
+                        margin: EdgeInsets.all(5),
+                        color: Colors.white,
+                      )),
+                  Flexible(
+                      flex: 1,
+                      child: Container(
+                        margin: EdgeInsets.all(5),
+                        color: Colors.purple,
+                      )),
+                ],
+              ),
+            ),
+            Flexible(
+                flex: 2,
+                child: Container(
+                  color: Colors.amber,
+                )),
+            Flexible(
+                flex: 1,
+                child: Container(
+                  color: Colors.blue,
+                )),
+          ],
+        ),
+      ),
+    );
+  }
+}
